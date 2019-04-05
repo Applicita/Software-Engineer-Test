be664624e51bf35676d23b3b90d244f3e0140e83
Adam Smith <adam.smith@applicita.com>
2019-04-05 10:20:26 +0100

    Baseline code for Tasks 2 and 3 added.

---
+++ Code/Applicita.Recruitment.Task2and3/Controllers/CustomerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using Applicita.Recruitment.Task2and3.Models;
+
+namespace Applicita.Recruitment.Task2and3.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post(Customer customer)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Code/Applicita.Recruitment.Task2and3/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Applicita.Recruitment.Task2and3.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string FavoriteProgrammingLanguage1 { get; set; }
+        public string FavoriteProgrammingLanguage2 { get; set; }
+    }
+}
